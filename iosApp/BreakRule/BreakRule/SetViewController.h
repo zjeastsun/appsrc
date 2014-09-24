025841b91140d187838a24059aff1b09494ba0f0
mac <54554333@qq.com>
2014-09-24 01:09:06 +0000

    。

---
+++ iosApp/BreakRule/BreakRule/SetViewController.h
@@ -0,0 +1,15 @@
+//
+//  setViewController.h
+//  BreakRule
+//
+//  Created by mac on 14-9-21.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetViewController : UIViewController
+- (IBAction)back:(id)sender;
+
+- (IBAction)UpdateDb:(id)sender;
+@end
