4c01d7af5787e641880ade0d9745455bc9ae2b1c
mac <54554333@qq.com>
2014-09-24 01:08:45 +0000

    。

---
+++ iosApp/BreakRule/BreakRule/eutils/SingletonIce.mm
@@ -0,0 +1,38 @@
+#import "SingletonIce.h"
+
+@implementation SingletonIce
+
+
++ (SingletonIce *)sharedInstance {
+    
+    static dispatch_once_t onceToken;
+    static SingletonIce *singletonInstanceIce= nil;
+    
+    dispatch_once(&onceToken, ^{
+        singletonInstanceIce = [[SingletonIce alloc] init];
+    });
+    
+    return singletonInstanceIce;
+}
+
+- (id)init {
+    
+    if (self = [super init]) {
+        _g_db = new CICEDBUtil();
+
+    }
+    
+    return self;
+    
+}
+
+- (void)unInit {
+    
+    if( _g_db != NULL )
+    {
+        delete _g_db;
+    }
+
+}
+
+@end
