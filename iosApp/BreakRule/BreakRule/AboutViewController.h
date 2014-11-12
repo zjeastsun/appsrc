1e796a6f1fb49eabef00e8b6536d8de23820471e
mac <54554333@qq.com>
2014-11-12 13:13:06 +0000

    增加about界面

---
+++ iosApp/BreakRule/BreakRule/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  BreakRule
+//
+//  Created by mac on 14-11-12.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)back:(id)sender;
+@end
